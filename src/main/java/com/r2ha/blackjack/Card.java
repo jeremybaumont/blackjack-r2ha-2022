a63f8772096f50ad95ca1b8e7f5328c97a465cb1
Jeremy Baumont <jeremy.baumont@gmail.com>
2022-10-11 14:56:44 +1100

    r - extract method suit and rank

--- src/main/java/com/r2ha/blackjack/Card.java
+++ src/main/java/com/r2ha/blackjack/Card.java
@@ -20,14 +20,14 @@ public class Card {
     public static String display(Card card) {
         String[] lines = new String[7];
         lines[0] = "┌─────────┐";
-        lines[1] = String.format("│%s%s       │", card.rank().display(), card.rank == Rank.TEN ? "" : " ");
+        lines[1] = String.format("│%s%s       │", card.rank().display(), card.rank() == Rank.TEN ? "" : " ");
         lines[2] = "│         │";
         lines[3] = String.format("│    %s    │", card.suit().symbol());
         lines[4] = "│         │";
         lines[5] = String.format("│       %s%s│", card.rank() == Rank.TEN ? "" : " ", card.rank().display());
         lines[6] = "└─────────┘";
 
-        Ansi.Color cardColor = card.suit.isRed() ? Ansi.Color.RED : Ansi.Color.BLACK;
+        Ansi.Color cardColor = card.suit().isRed() ? Ansi.Color.RED : Ansi.Color.BLACK;
         return ansi()
                 .fg(cardColor).toString()
                 + String.join(ansi().cursorDown(1)
