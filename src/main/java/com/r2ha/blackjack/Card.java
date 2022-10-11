ae93db5e2e838b73a431f3b3ea3c00e1192db8a9
Jeremy Baumont <jeremy.baumont@gmail.com>
2022-10-11 14:59:06 +1100

    r - make static display

--- src/main/java/com/r2ha/blackjack/Card.java
+++ src/main/java/com/r2ha/blackjack/Card.java
@@ -1,7 +1,5 @@
 package com.r2ha.blackjack;
 
-import org.fusesource.jansi.Ansi;
-
 import static org.fusesource.jansi.Ansi.ansi;
 
 public class Card {
@@ -15,24 +13,6 @@ public class Card {
 
     public int rankValue() {
         return rank.value();
-    }
-
-    public static String display(Card card) {
-        String[] lines = new String[7];
-        lines[0] = "┌─────────┐";
-        lines[1] = String.format("│%s%s       │", card.rank().display(), card.rank() == Rank.TEN ? "" : " ");
-        lines[2] = "│         │";
-        lines[3] = String.format("│    %s    │", card.suit().symbol());
-        lines[4] = "│         │";
-        lines[5] = String.format("│       %s%s│", card.rank() == Rank.TEN ? "" : " ", card.rank().display());
-        lines[6] = "└─────────┘";
-
-        Ansi.Color cardColor = card.suit().isRed() ? Ansi.Color.RED : Ansi.Color.BLACK;
-        return ansi()
-                .fg(cardColor).toString()
-                + String.join(ansi().cursorDown(1)
-                                    .cursorLeft(11)
-                                    .toString(), lines);
     }
 
     public Suit suit() {
